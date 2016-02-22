1f39b5e729f640fcb2848b9d899e1d1ac2f557b4
wodrow <wodrow451611cv@gmail.com>
2016-02-22 18:26:44 +0800

    init

---
+++ Public/Common/common.js
@@ -0,0 +1,39 @@
+//右侧iframe框架高度自适应
+$(function(){
+    var height = $(".tabs-panels").innerHeight();
+    height = height - 20;
+    $("#mainFrame").css("height",height);
+})
+
+// dataTables 汉化
+var dataTables_zh_CN = {
+    "sProcessing": "处理中...",
+    "sLengthMenu": "显示 _MENU_ 项结果",
+    "sZeroRecords": "没有匹配结果",
+    "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+    "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+    "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+    "sInfoPostFix": "",
+    "sSearch": "搜索:",
+    "sUrl": "",
+    "sEmptyTable": "表中数据为空",
+    "sLoadingRecords": "载入中...",
+    "sInfoThousands": ",",
+    "oPaginate": {
+        "sFirst": "首页",
+        "sPrevious": "上页",
+        "sNext": "下页",
+        "sLast": "末页"
+    },
+    "oAria": {
+        "sSortAscending": ": 以升序排列此列",
+        "sSortDescending": ": 以降序排列此列"
+    }
+}
+
+//日期转换成时间戳
+function formatTime(time){
+    var time = Date.parse(new Date(time));
+    time = time / 1000;
+    return time;
+}
