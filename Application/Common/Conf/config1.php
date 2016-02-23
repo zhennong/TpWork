6455e8c21d77c9c3b72679413274d5a568ac5aaa
wodrow <wodrow451611cv@gmail.com>
2016-02-23 10:45:04 +0800

    init

--- Application/Common/Conf/config1.php
+++ Application/Common/Conf/config1.php
@@ -5,3 +5,17 @@
  * Date: 2/23/16
  * Time: 8:40 AM
  */
+
+return [
+    //调试模式
+    'LOG_RECORD'            =>  true,  // 进行日志记录
+    'LOG_EXCEPTION_RECORD'  =>  true,    // 是否记录异常信息日志
+    'LOG_LEVEL'             =>  'EMERG,ALERT,CRIT,ERR,WARN,NOTIC,INFO,DEBUG,SQL',  // 允许记录的日志级别
+    'DB_FIELDS_CACHE'       =>  false, // 字段缓存信息
+    'DB_DEBUG'				=>  true, // 开启调试模式 记录SQL日志
+    'TMPL_CACHE_ON'         =>  false,        // 是否开启模板编译缓存,设为false则每次都会重新编译
+    'TMPL_STRIP_SPACE'      =>  false,       // 是否去除模板文件里面的html空格与换行
+    'SHOW_ERROR_MSG'        =>  true,    // 显示错误信息
+    'URL_CASE_INSENSITIVE'  =>  false,  // URL区分大小写
+    'SHOW_PAGE_TRACE'       =>  true,
+];
