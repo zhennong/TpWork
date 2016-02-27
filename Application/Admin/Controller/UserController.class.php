589688160fb79655eb9b62aa10fbbd8c6820a7bb
wodrow <wodrow451611cv@gmail.com>
2016-02-27 15:20:58 +0800

    init

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -13,39 +13,59 @@ use Common\Tools;
 
 class UserController extends AuthController
 {
-    public function user_index(){
+    public function user_index()
+    {
         $this->display('Index:index');
     }
 
     /**
      * 用户管理
      */
-    public function user_manage(){
-        if(IS_POST){
-            Tools::_vp($_POST);
+    public function user_manage()
+    {
+        if (IS_POST) {
+            if ($id = I("post.delete_id")) {
+                if(D('AdminUser')->delete($id)){
+                    echo 1;
+                }
+            } else {
+                $AdminUser = D('AdminUser');
+                $x = $AdminUser->create();
+                if ($x) {
+                    if ($x['id'] > 0) {
+                        $y = $AdminUser->save();
+                    } else {
+                        $y = $AdminUser->add();
+                    }
+                    echo $y;
+                } else {
+                    echo $AdminUser->getError();
+                }
+            }
             exit();
         }
-        $user_list = $this->getAdminUserDetail(null,['group_detail']);
-        $this->assign(['user_list'=>$user_list]);
-        Tools::_vp(D('AdminUser')->select());
+        $user_list = $this->getAdminUserDetail(null, ['group_detail']);
+        $this->assign(['user_list' => $user_list]);
         $this->display();
     }
 
     /**
      * 用户组管理
      */
-    public function group_manage(){
+    public function group_manage()
+    {
         $group_list = $this->getAuthGroupDetail();
-        $this->assign(['group_list'=>$group_list]);
+        $this->assign(['group_list' => $group_list]);
         $this->display();
     }
 
     /**
      * 节点管理
      */
-    public function node_manage(){
+    public function node_manage()
+    {
         $node_tree = Tools::list2tree($this->authAll);
-        $this->assign(['node_tree'=>$node_tree]);
+        $this->assign(['node_tree' => $node_tree]);
         $this->display();
     }
 }
--- Application/Admin/Model/AdminUserModel.class.php
+++ Application/Admin/Model/AdminUserModel.class.php
@@ -11,5 +11,27 @@ namespace Admin\Model;
 
 class AdminUserModel extends AdminModel
 {
-    public $tableName = 'admin';
+    protected $tableName = 'admin';
+
+    //自动完成
+    protected $_auto = [
+        ['password','md5',3,'function'],
+        ['login_time','time',3,'function'],
+        ['login_ip','get_client_ip',3,'function'],
+        ['create_time','time',1,'function']
+    ];
+
+    //自动验证
+    protected $_validate = [
+        ['account','require','帐号必须！'],
+        ['account','','帐号名称已经存在！',0,'unique',1],
+        ['password',"6,18",'密码长度不正确!',0,'length'],
+        ['repassword','password','确认密码不正确!',0,'confirm'],
+        ['mobile','require','手机号必须！'],
+        ['mobile','11',"手机号不正确!",0,'length'],
+        ['mobile','','手机号已经存在！',0,'unique',1],
+        ['email','require','邮箱必须！'],
+        ['email','email',"邮箱格式不正确",0,'email'],
+        ['email','','邮箱已经存在！',0,'unique',1]
+    ];
 }
