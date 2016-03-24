e819cacd38d4d252f651ace091729d05b249eef0
wodrow <wodrow451611cv@gmail.com>
2016-03-24 15:13:31 +0800

    update

--- Application/Common/Controller/CommonController.class.php
+++ Application/Common/Controller/CommonController.class.php
@@ -13,10 +13,25 @@ use Think\Controller;
 abstract class CommonController extends Controller
 {
     public $route=''; //  路由地址
+    protected $MallDb;
+    protected $malldb; //商城数据库链接
 
     public function _initialize(){
         // 获取路由地址
         $this->route = MODULE_NAME . "/" . CONTROLLER_NAME . "/" . ACTION_NAME;
         M()->execute("SET NAMES UTF8");
+
+        $this->getMallDb();
+    }
+
+    /**
+     * 获取商城数据库连接
+     */
+    private function getMallDb(){
+        if(!$this->MallDb){
+            $MallDb = new MallDb();
+            $this->MallDb = $MallDb;
+            $this->malldb = $MallDb->db;
+        }
     }
 }
