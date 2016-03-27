a14cba8a876584215c2230a11911d5ca8c15e4ac
junqianhen@gmail.com <yibeizi21>
2016-03-28 03:55:20 +0800

    ask

--- Application/Admin/Controller/AskController.class.php
+++ Application/Admin/Controller/AskController.class.php
@@ -21,6 +21,7 @@ class AskController extends AuthController
          * 查询表信息
          */
         $list = $Dao->query("select a.id,a.content,a.addtime,a.perfect_answer_id,a.areaid,a.catid,b.cat_name,c.nickname from destoon_appknow_question_ask as a left join destoon_appknow_category as b on a.catid = b.id left join destoon_appknow_member_profile as c on c.userid = a.uid");
+
         foreach ($list as $k => $v) {
             $question_list[$k]['id'] = $v['id'];
             $question_list[$k]['content'] = $v['content'];
@@ -30,9 +31,60 @@ class AskController extends AuthController
             $question_list[$k]['cat_name'] = $v['cat_name'];
             $question_list[$k]['nickname'] = $v['nickname'];
             $arealist = getAreaFullNameFromAreaID($v['areaid']);
-            $question_list[$k]['area'] = arr2str($arealist,'');
+            $question_list[$k]['area'] = arr2str($arealist, '');
         }
         $this->assign(['question_list' => $question_list]);
         $this->display();
     }
+
+    /*
+     * 查看提问
+     */
+
+    function view_question()
+    {
+        $data = array();
+        $data["question_profile"] = array(
+            "id" => I('get.id'),
+            "nickname" => I('get.nickname'),
+            "content" => I('get.content'),
+            "addtime" => I('get.addtime'),
+            "cat_name" => I('get.cat_name'),
+        );
+        dump($data);
+        $this->display();
+    }
+
+    /*
+     * 问题修改
+     */
+    function edit_question()
+    {
+        $data = array();
+        $data["question_profile"] = array(
+            "id" => I('get.id'),
+            "nickname" => I('get.nickname'),
+            "content" => I('get.content'),
+            "addtime" => I('get.addtime'),
+            "cat_name" => I('get.cat_name'),
+        );
+        dump($data);
+        $this->display();
+    }
+
+    public function delete_question()
+    {
+        $id = I('get.id');
+        if (!empty($id)) {
+            $result = D('Ask')->delete($id);
+            if ($result) {
+                $this->ajaxReturn(1);
+            } else {
+                $this->ajaxReturn(0);
+            }
+        } else {
+            $this->ajaxReturn(2); //参数异常
+        }
+    }
+
 }
