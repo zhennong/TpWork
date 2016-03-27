cecb6bf0e93e228e0cce5f513b4f516e841d6409
junqianhen@gmail.com <yibeizi21>
2016-03-28 05:55:44 +0800

    update AskController

--- Application/Admin/Controller/AskController.class.php
+++ Application/Admin/Controller/AskController.class.php
@@ -16,11 +16,11 @@ class AskController extends AuthController
         /*
         * 获取提问列表信息
         */
-        $Dao = D('Ask');
+        $Ask = D('Ask');
         /*
          * 查询表信息
          */
-        $list = $Dao->query("select a.id,a.content,a.addtime,a.perfect_answer_id,a.areaid,a.catid,b.cat_name,c.nickname from destoon_appknow_question_ask as a left join destoon_appknow_category as b on a.catid = b.id left join destoon_appknow_member_profile as c on c.userid = a.uid");
+        $list = $Ask->query("select a.id,a.content,a.addtime,a.perfect_answer_id,a.areaid,a.catid,b.cat_name,c.nickname from destoon_appknow_question_ask as a left join destoon_appknow_category as b on a.catid = b.id left join destoon_appknow_member_profile as c on c.userid = a.uid GROUP BY a.addtime");
 
         foreach ($list as $k => $v) {
             $question_list[$k]['id'] = $v['id'];
@@ -43,15 +43,24 @@ class AskController extends AuthController
 
     function view_question()
     {
-        $data = array();
-        $data["question_profile"] = array(
-            "id" => I('get.id'),
-            "nickname" => I('get.nickname'),
-            "content" => I('get.content'),
-            "addtime" => I('get.addtime'),
-            "cat_name" => I('get.cat_name'),
-        );
-        dump($data);
+        $id = I('get.id');
+        if(!empty($id)){
+           $Ask = D('Ask');
+           $map['id'] = $id;
+           $sql = $Ask->query("select a.id,a.content,a.addtime,a.perfect_answer_id,a.areaid,a.catid,b.id AS cate_id,b.cat_name,c.nickname from destoon_appknow_question_ask as a left join destoon_appknow_category as b on a.catid = b.id left join destoon_appknow_member_profile as c on c.userid = a.uid WHERE a.id=$id");
+           foreach($sql as$k=>$v){
+               $view_list[$k]['id'] = $v['id'];
+               $view_list[$k]['content'] = $v['content'];
+               $view_list[$k]['addtime'] = date('Y-m-d h:i:s', $v['addtime']);
+               $view_list[$k]['perfect_answer_id'] = $v['perfect_answer_id'];
+               $view_list[$k]['areaid'] = $v['areaid'];
+               $view_list[$k]['cat_name'] = $v['cat_name'];
+               $view_list[$k]['nickname'] = $v['nickname'];
+               $arealist = getAreaFullNameFromAreaID($v['areaid']);
+               $view_list[$k]['area'] = arr2str($arealist, '');
+           }
+            $this->assign(['view_list' => $view_list]);
+       }
         $this->display();
     }
 
@@ -60,15 +69,24 @@ class AskController extends AuthController
      */
     function edit_question()
     {
-        $data = array();
-        $data["question_profile"] = array(
-            "id" => I('get.id'),
-            "nickname" => I('get.nickname'),
-            "content" => I('get.content'),
-            "addtime" => I('get.addtime'),
-            "cat_name" => I('get.cat_name'),
-        );
-        dump($data);
+        $id = I('get.id');
+        if(!empty($id)){
+            $Ask = D('Ask');
+            $map['id'] = $id;
+            $sql = $Ask->query("select a.id,a.content,a.addtime,a.catid,b.id AS cate_id,b.cat_name,c.nickname from destoon_appknow_question_ask as a left join destoon_appknow_category as b on a.catid = b.id left join destoon_appknow_member_profile as c on c.userid = a.uid WHERE a.id=$id");
+            foreach($sql as$k=>$v){
+                $edit_question[$k]['id'] = $v['id'];
+                $edit_question[$k]['nickname'] = $v['nickname'];
+                $edit_question[$k]['content'] = $v['content'];
+                $edit_question[$k]['addtime'] = date('Y-m-d h:i:s', $v['addtime']);
+                $edit_question[$k]['cat_name'] = $v['cat_name'];
+                $edit_question[$k]['catid'] = $v['catid'];
+                $arealist = getAreaFullNameFromAreaID($v['areaid']);
+                $edit_question[$k]['area'] = arr2str($arealist, '');
+            }
+            $this->assign(['edit_question' => $edit_question]);
+
+        }
         $this->display();
     }
 
