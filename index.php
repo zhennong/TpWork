c849758348775bf37721031f8e260e8736076373
wodrow <wodrow451611cv@gmail.com>
2016-03-28 17:43:48 +0800

    update

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if(version_compare(PHP_VERSION,'5.4.0','<'))  die('require PHP > 5.4.0 !');
 define('APP_ROOT',dirname(__FILE__)."/");
 
 // 定义默认模块
-//define('BIND_MODULE','Admin');
+// define('BIND_MODULE','Admin');
 
 // 定义应用目录
 define('APP_PATH',APP_ROOT.'/Application/');
