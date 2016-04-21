a5b280d41c802379dd833115387c09bb43791fd6
iredbaby <595697511@qq.com>
2016-04-21 11:19:50 +0800

    update

--- Application/Admin/Controller/UpgradeController.class.php
+++ Application/Admin/Controller/UpgradeController.class.php
@@ -7,7 +7,27 @@ class UpgradeController extends AuthController {
     public function softSetting(){
         //判断是否编辑xml
         if(I('get.action') == 'edit'){
-            dump(2);
+            $softname = I('get.softname');
+            $softversion = I('get.softversion');
+            $softfilename = I('get.softfilename');
+            $softsize = I('get.softsize');
+
+            $dom = new \DOMDocument();
+            $dom->load($_SERVER['DOCUMENT_ROOT'].'/touchknow/upgrade.xml');
+            $file = $dom->getElementsByTagName("file");
+            $file->item(0)->setAttribute("name",$softname);
+
+            $news = $dom->getElementsByTagName("news");
+            $news->item(0)->setAttribute("version",$softversion);
+            $news->item(0)->setAttribute("android_filename",$softfilename);
+
+            $size = $dom->getElementsByTagName("size");
+            $size->item(0)->setAttribute("android",$softsize);
+
+            $result = $dom->save($_SERVER['DOCUMENT_ROOT'].'/touchknow/upgrade.xml');
+            if($result !== false){
+                $this->ajaxReturn(200);
+            }
         }
 
         $info = simplexml_load_file($_SERVER['DOCUMENT_ROOT'].'/touchknow/upgrade.xml');
@@ -17,6 +37,8 @@ class UpgradeController extends AuthController {
         $android_size=$info->size['android'];//andoird文件大小.
         $version=$info->news['version']; //版本号
         $updateFileName=$info->file['name'];//客户端名称.
+
+        //$android_size = Tools::getRealSize($android_size);
 
         $this->assign(['iphone_filename'=>$iphone_filename,'android_filename'=>$android_filename,'iphone_size'=>$iphone_size,'android_size'=>$android_size,'version'=>$version,'name'=>$updateFileName]);
         $this->display();
