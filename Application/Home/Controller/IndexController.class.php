a8a700cd8e511bfdb862bd6bc937ec79d34de9b7
iredbay <admin951753>
2016-07-01 17:06:16 +0800

    默认的

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -29,12 +29,9 @@ class IndexController extends CommonController {
                         $api->getLastLoginTime($x[0]['userid']);
 
                         //登录积分设置 每天累加一分
-                        $mem_info = $api->getUserFromMobile(I('get.mobile'));
-                        $last_time = intval($mem_info[0]['last_login_time']);
+                        $last_time = $x[0]['last_login_time'];
                         $timestamp = time() - $last_time;
-
-                        $api->putLog('sql',is_int($last_time));
-
+                        $api->putLog('sql',$timestamp);
                         if($timestamp > 86400){  //24h 后才可以累加积分
                             $api->addScore($x[0]['userid'],'sa_login');
                         }
