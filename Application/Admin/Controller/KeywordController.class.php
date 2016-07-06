70a01d836eb61b901e172bb9263bb3631907e221
iredbay <admin951753>
2016-07-06 11:46:39 +0800

    默认的

---
+++ Application/Admin/Controller/KeywordController.class.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wodrow
+ * Date: 2/24/16
+ * Time: 8:52 AM
+ */
+
+namespace Admin\Controller;
+
+use Common\Controller\CommonController;
+use Common\Tools;
+
+class KeywordController extends AuthController
+{
+    /**
+     * 关键词列表
+     */
+    public function keyword_list(){
+        $sql = "SELECT * FROM destoon_appknow_keyword";
+
+        $data = M('')->query($sql);
+
+        $this->assign(['data'=>$data]);
+        $this->display();
+    }
+
+    /**
+     * 关键词删除
+     */
+    public function keyword_delete(){
+        $id = I('get.id');
+        if (!empty($id)){
+            $result = D('keyword')->delete($id);
+            if ($result) {
+                $this->ajaxReturn(1);
+            } else {
+                $this->ajaxReturn(0);
+            }
+        } else {
+            $this->ajaxReturn(2); //参数异常
+        }
+    }
+
+    /**
+     * 关键词添加
+     */
+    public function keyword_add(){
+        $opt = I('get.action');
+        if($opt == 'add'){
+            $data = array();
+            $data['keyword'] = I('get.keyword');
+            $data['stars'] = I('get.stars');
+            $data['addtime'] = time();
+
+            $result = D('keyword')->add($data);
+            if($result){
+                $this->ajaxReturn(1);
+            }else{
+                $this->ajaxReturn(0);
+            }
+        }
+        $this->display();
+    }
+}
---
+++ Application/Admin/Model/KeywordModel.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 16-3-24
+ * Time: 下午2:51
+ */
+
+namespace Admin\Model;
+
+
+
+class KeywordModel extends AdminModel
+{
+    protected $tableName = 'keyword';
+}
