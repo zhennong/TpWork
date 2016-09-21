aeeca50faa44b8f06d0f9374d1584e1021c59cac
iredbaby <595697511@qq.com>
2016-09-21 14:53:04 +0800

    农药查询接口样式修改

--- API/show_7.php
+++ API/show_7.php
@@ -28,7 +28,7 @@ function replace($str){
     $str = str_replace("</head>","<style>body{background: #fff;padding-top: 53px;}.Noprint{display: none;}.fixed{width:100%;line-height:40px;position: fixed;left:0;top:0;background: #0d8c1f;z-index: 9999;box-shadow: 3px 3px 3px #ccc;}.fixed a{line-height: 40px;font-size:14px;margin-left: 10px;color:#fff;}#tsearch{border:0px;padding:0px;margin:0px;border-left:1px solid #ccc;border-top:1px solid #ccc;}#tsearch td{padding:3px 5px;font-size: 14px;border-right:1px solid #ccc;border-bottom:1px solid #ccc;line-height: 25px;}#tsearch td span{margin-left: 5px;}</style></head>",$str);
     $str = str_replace("#9b0b0b","#fff",$str);
     $str = str_replace("<title>","<meta name='viewport' content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'><title>",$str);
-    $str = str_replace("</body>","<div class='fixed'><a href='javascript:;' onclick='history.go(-1)'>返回上一页 >></a></div></body>",$str);
+    $str = str_replace("</body>","<div class='fixed'><a href='javascript:;' onclick='history.go(-1)'>返回上一页  >></a></div></body>",$str);
     $str = str_replace("1170","100%",$str);
     $str = str_replace("985","100%",$str);
     $str = str_replace("622px","100%",$str);
